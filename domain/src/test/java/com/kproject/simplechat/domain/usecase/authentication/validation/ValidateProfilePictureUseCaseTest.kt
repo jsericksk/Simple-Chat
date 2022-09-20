3ef69afe93d8cb3a6d6a20a6b86809d53c742e57
jsericksk <jsericksk@gmail.com>
2022-09-20 15:04:26 -0300

    tests: Created tests for ValidateProfilePictureUseCase

---
+++ domain/src/test/java/com/kproject/simplechat/domain/usecase/authentication/validation/ValidateProfilePictureUseCaseTest.kt
@@ -0,0 +1,37 @@
+package com.kproject.simplechat.domain.usecase.authentication.validation
+
+import com.kproject.simplechat.commom.validation.ValidationState
+import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Test
+
+class ValidateProfilePictureUseCaseTest {
+    private lateinit var validateProfilePictureUseCase: ValidateProfilePictureUseCase
+
+    @Before
+    fun setUp() {
+        validateProfilePictureUseCase = ValidateProfilePictureUseCaseImpl()
+    }
+
+    @Test
+    fun `profile picture not selected returns ProfilePictureNotSelected error`() {
+        val profilePicture = ""
+
+        val expected = ValidationState.ProfilePictureNotSelected
+
+        val result = validateProfilePictureUseCase(profilePicture)
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `profile picture selected returns Success`() {
+        val profilePicture = "imageUri"
+
+        val expected = ValidationState.Success
+
+        val result = validateProfilePictureUseCase(profilePicture)
+
+        assertEquals(expected, result)
+    }
+}
