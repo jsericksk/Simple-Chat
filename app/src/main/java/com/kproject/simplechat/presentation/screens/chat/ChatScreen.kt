116e74efcec1fc847fc80c32e33f5682b4aa9905
jsericksk <jsericksk@gmail.com>
2022-10-05 20:54:59 -0300

    refactor: Added one-line limit on username text in ChatScreen TopBar

--- app/src/main/java/com/kproject/simplechat/presentation/screens/chat/ChatScreen.kt
+++ app/src/main/java/com/kproject/simplechat/presentation/screens/chat/ChatScreen.kt
@@ -159,6 +159,7 @@ private fun TopBar(
             color = Color.White,
             fontSize = 20.sp,
             fontWeight = FontWeight.Bold,
+            maxLines = 1,
             modifier = Modifier
                 .fillMaxWidth()
         )
